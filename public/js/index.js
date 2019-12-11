3ee33ec62a4f9abfe1437424e5934206e739d788
Malith <malith.rajapakse@gmail.com>
2019-12-12 10:13:41 +1100

    [Bugfix] All form submits calls login module
    Fixes form submit bug which always call the login module. Login form
    now has an additional CSS class a specific query selector

--- public/js/index.js
+++ public/js/index.js
@@ -5,7 +5,7 @@ import { displayMap } from './mapbox';
 
 //DOM ELements
 const mapBox = document.getElementById('map');
-const loginForm = document.querySelector('.form');
+const loginForm = document.querySelector('.form--login');
 const logOutBtn = document.querySelector('.nav__el--logout');
 //Values
 
